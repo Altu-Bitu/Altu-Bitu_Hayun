4bf16dc927d91fc5ff02e427372fb03aea13578b
haayun <kng06325@gmail.com>
2021-10-11 15:54:26 +0900

    [분할 정복] 10월 11일 - Update

--- 10월 5일 - 분할 정복/21314.cpp
+++ 10월 5일 - 분할 정복/21314.cpp
@@ -8,13 +8,17 @@ using namespace std;
 void getMax(string s) {	
 	string max_mk = "";		//바로 push back할 수 있도록 string으로
 	int last_k = -1;
+	string temp = "";	//M을 변환해 담아둘 string
 	for (int i = 0; i < s.size(); i++) {
 		if (s[i] == 'K') {	//K가 나오면 5를 붙이고
 			max_mk.push_back('5');
-			for (int j = 0; j < i - last_k -1; j++) {	//M 개수만큼 0을 붙인다 (이전 K 위치로 M 개수 파악)
-				max_mk.push_back('0');
-			}
+			max_mk += temp;
+			temp = "";
 			last_k = i;	//K 인덱스 업데이트
+
+		}
+		else {
+			temp.push_back('0');
 		}
 	}
 	for(int i = last_k + 1; i < s.size(); i++)	//남은 M의 개수는 전부 1로 붙인다
@@ -26,16 +30,19 @@ void getMax(string s) {
 void getMin(string s) {	
 	string min_mk = "";
 	int last_k = -1;
+	string temp = "";	//M을 변환해 담아둘 string
 	for (int i = 0; i < s.size(); i++) {
 		if (s[i] == 'K') {	//K가 나오면
-			for (int j = 0; j < i - last_k - 1; j++) {	//M의 개수만큼 10...0 붙인다
-				if (j == 0)
-					min_mk.push_back('1');
-				else
-					min_mk.push_back('0');
-			}
+			min_mk += temp;
 			min_mk.push_back('5');	//5를 붙인다.
+			temp = "";
 			last_k = i;		//K 인덱스 업데이트
+		}
+		else {
+			if (temp.size() == 0)
+				temp.push_back('1');
+			else
+				temp.push_back('0');
 		}
 	}
 	for (int i = last_k + 1; i < s.size(); i++) {	//남은 M의 개수만큼 10...0 붙인다
@@ -46,7 +53,6 @@ void getMin(string s) {
 	}
 	cout << min_mk << endl;
 }
-
 
 int main() {
 	string s;
