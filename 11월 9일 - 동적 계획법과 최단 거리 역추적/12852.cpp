06e07696b23ef69c52b9b079eb5fceb3f39d5fcf
haayun <kng06325@gmail.com>
2021-11-15 23:21:18 +0900

    [동적 계획법과 최단 거리 역추적] 11월 15일

--- 11월 9일 - 동적 계획법과 최단 거리 역추적/12852.cpp
+++ 11월 9일 - 동적 계획법과 최단 거리 역추적/12852.cpp
@@ -5,6 +5,26 @@ using namespace std;
 
 const int SIZE = 1000001;
 
+void makeOne(int n, vector<int> &dp, vector<int> &path) {
+
+	//연산
+	dp[1] = 0;	//타겟은 1
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;	//3번 연산
+		path[i] = i - 1;
+
+		if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {	//1번 연산
+			dp[i] = dp[i / 3] + 1;
+			path[i] = i / 3;
+		}
+		if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {	//2번 연산
+			dp[i] = dp[i / 2] + 1;
+			path[i] = i / 2;
+		}
+	}
+}
+
 int main() {
 	vector<int> dp(SIZE, SIZE);	//연산 최솟값 저장 배열
 	vector<int> path(SIZE);		//경로 저장 배열
@@ -14,23 +34,7 @@ int main() {
 	cin >> n;
 
 	//연산
-	dp[1] = 0;	//타겟은 1
-
-	for (int i = 2; i <= n; i++) {
-		
-		dp[i] = dp[i - 1] + 1;	//3번 연산
-		path[i] = i - 1;		
-		
-		if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {	//1번 연산
-			dp[i] = dp[i / 3] + 1;
-			path[i] = i / 3;
-		}
-		if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {	//2번 연산
-			dp[i] = dp[i / 2] + 1;
-			path[i] = i / 2;
-		}
-		
-	}
+	makeOne(n, dp, path);
 
 	//출력
 	cout << dp[n] << '\n';	//연산 횟수의 최솟값
--- 11월 9일 - 동적 계획법과 최단 거리 역추적/2615.cpp
+++ 11월 9일 - 동적 계획법과 최단 거리 역추적/2615.cpp
@@ -15,11 +15,11 @@ bool check_five(int x, int y, int color) {	//오목인지 확인하는 함수
 	for (int i = 0; i < 4; i++) {
 		int cnt = 0;
 		int nx = x + dx[i], ny = y + dy[i];
-		
-		if (nx < 0 || nx >= 19 || ny < 0 || ny >= 19)	//범위를 벗어난다면 패스
-			continue;
 
 		while (true) {
+			if (nx < 0 || nx >= 19 || ny < 0 || ny >= 19)	//범위를 벗어난다면 패스
+				break;
+
 			if (board[nx][ny] == color) {
 				cnt++;
 				nx += dx[i];
@@ -28,8 +28,12 @@ bool check_five(int x, int y, int color) {	//오목인지 확인하는 함수
 			else
 				break;
 		}
-		if (cnt == 4)	//오목 완성
+		if (cnt == 4) {	//오목 완성
+			int left_x = x - dx[i], left_y = y - dy[i];
+			if (left_x >= 0 && left_x < 19 && left_y >= 0 && left_y < 19 && board[left_x][left_y] == color) //시작한 바둑알의 역방향이 범위 내인지, 같은 색의 바둑알이 있는지 확인 -> 그렇다면 육목이 된다
+				continue;
 			return true;
+		}
 	}
 	return false;
 }
