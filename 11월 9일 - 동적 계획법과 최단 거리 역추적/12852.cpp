fce49c263f977049c144dc43a856f6b6f1bee118
haayun <kng06325@gmail.com>
2021-11-16 00:37:46 +0900

    [동적 계획법과 최단 거리 역추적] 11월 15일 - Update

--- 11월 9일 - 동적 계획법과 최단 거리 역추적/12852.cpp
+++ 11월 9일 - 동적 계획법과 최단 거리 역추적/12852.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 const int SIZE = 1000001;
 
-void makeOne(int n, vector<int> &dp, vector<int> &path) {
-
-	//연산
+vector<int> makeOne(int n, vector<int> &path) {
+	
+	vector<int> dp(SIZE, SIZE);	//연산 최솟값 저장 배열
+	vector<int> result;
 	dp[1] = 0;	//타겟은 1
 
 	for (int i = 2; i <= n; i++) {
@@ -23,10 +24,15 @@ void makeOne(int n, vector<int> &dp, vector<int> &path) {
 			path[i] = i / 2;
 		}
 	}
+	while (n != 0) {
+		result.push_back(n);
+		n = path[n];
+	}
+	return result;
 }
 
 int main() {
-	vector<int> dp(SIZE, SIZE);	//연산 최솟값 저장 배열
+	
 	vector<int> path(SIZE);		//경로 저장 배열
 
 	int n;
@@ -34,13 +40,12 @@ int main() {
 	cin >> n;
 
 	//연산
-	makeOne(n, dp, path);
+	vector<int> result = makeOne(n, path);
 
 	//출력
-	cout << dp[n] << '\n';	//연산 횟수의 최솟값
-	while (n != 0) {
-		cout << n << " ";	//1로 만드는 방법에 포함되는 수
-		n = path[n];
+	cout << result.size() - 1 << '\n';	//연산 횟수의 최솟값
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << " ";
 	}
 	return 0;
 }
