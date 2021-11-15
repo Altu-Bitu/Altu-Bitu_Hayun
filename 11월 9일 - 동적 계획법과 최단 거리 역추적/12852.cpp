d78d7f68db66ad884592a31c0a18ce8c26233742
haayun <kng06325@gmail.com>
2021-11-15 21:54:06 +0900

    [동적 계획법과 최단 거리 역추적] 11월 15일 - ing

---
+++ 11월 9일 - 동적 계획법과 최단 거리 역추적/12852.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int SIZE = 1000001;
+
+int main() {
+	vector<int> dp(SIZE, SIZE);	//연산 최솟값 저장 배열
+	vector<int> path(SIZE);		//경로 저장 배열
+
+	int n;
+	//입력
+	cin >> n;
+
+	//연산
+	dp[1] = 0;	//타겟은 1
+
+	for (int i = 2; i <= n; i++) {
+		
+		dp[i] = dp[i - 1] + 1;	//3번 연산
+		path[i] = i - 1;		
+		
+		if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {	//1번 연산
+			dp[i] = dp[i / 3] + 1;
+			path[i] = i / 3;
+		}
+		if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {	//2번 연산
+			dp[i] = dp[i / 2] + 1;
+			path[i] = i / 2;
+		}
+		
+	}
+
+	//출력
+	cout << dp[n] << '\n';	//연산 횟수의 최솟값
+	while (n != 0) {
+		cout << n << " ";	//1로 만드는 방법에 포함되는 수
+		n = path[n];
+	}
+	return 0;
+}
--- 11월 9일 - 동적 계획법과 최단 거리 역추적/9252.cpp
+++ 11월 9일 - 동적 계획법과 최단 거리 역추적/9252.cpp
@@ -8,13 +8,14 @@ using namespace std;
 //동적계획법 역추적
 //메모리가..... 괜찮은건가
 
-void lcs(int n, int m, string s1, string s2, vector<vector<string>> &path) {
+string lcs(int n, int m, string s1, string s2, vector<vector<string>> &path) {
 	vector<vector<int>> dp(n, vector<int>(m, 0));	//LCS DP 배열
 	for (int i = 1; i < n; i++) {
 		for (int j = 1; j < m; j++) {
 			if (s1[i - 1] == s2[j - 1]) {	//두 문자가 서로 같다면 공통 부분 문자열에 추가됨
 				dp[i][j] = dp[i - 1][j - 1] + 1;
-				path[i][j] = path[i - 1][j - 1] + s1[i - 1];	//추가되기 전 string + 같은 문자
+				path[i][j] = path[i - 1][j - 1];
+				path[i][j].append({ s1[i - 1] });	//추가되기 전 string + 같은 문자
 			}
 			else {	//두 문자가 서로 다르다면 그 전의 길이 최댓값 그대로, string도 그대로
 				if (dp[i - 1][j] > dp[i][j - 1]) {
@@ -28,6 +29,7 @@ void lcs(int n, int m, string s1, string s2, vector<vector<string>> &path) {
 			}
 		}
 	}
+	return path[n - 1][m - 1];
 }
 
 int main() {
@@ -43,8 +45,7 @@ int main() {
 	vector<vector<string>> path(n, vector<string>(m));
 	
 	//연산
-	lcs(n, m, s1, s2, path);
-	string result = path[n - 1][m - 1];
+	string result = lcs(n, m, s1, s2, path);
 
 	//출력
 	if (result.size() > 0)
