212742a86a7d5af16b9f265eb0e387d257527806
haayun <kng06325@gmail.com>
2021-12-08 00:57:26 +0900

    [최소 신장 트리] 12월 7일 - 추가 제출

--- 11월 30일 - 최소 신장 트리/17472.cpp
+++ 11월 30일 - 최소 신장 트리/17472.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+//풀이를 봐도 어려운 문제... 방법이라도 익혀두기
+//DFS(BFS) + MST 
+
 typedef tuple<int, int, int> tp;
 
 vector<int> parent;
@@ -13,7 +16,7 @@ vector<vector<int>> board;
 int dr[4] = { -1, 1, 0, 0 };
 int dc[4] = { 0, 0, -1, 1 };
 
-void dfs(int n, int m, int row, int col, int mark) {	//해당 구역의 섬을 mark로 표시
+void dfs(int n, int m, int row, int col, int mark) {	//해당 구역의 섬을 mark로 표시하는 함수
 	if (row < 0 || row >= n || col<0 || col>=m || board[row][col] != 1)	//범위를 벗어나거나 탐색할 섬이 아님
 		return;
 
@@ -22,11 +25,11 @@ void dfs(int n, int m, int row, int col, int mark) {	//해당 구역의 섬을 m
 		dfs(n, m, row + dr[i], col + dc[i], mark);	//상하좌우, 재귀로 호출
 }
 
-int markIsland(int n, int m) {	//모든 섬을 탐색하며 구분 짓기 (mark 증가하면서)
+int markIsland(int n, int m) {	//모든 섬을 탐색하며 구분 짓기 (mark 2부터)
 	int mark = 1;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (board[i][j] == 1)	//탐색하지 않은 섬
+			if (board[i][j] == 1)			//탐색하지 않은 섬
 				dfs(n, m, i, j, ++mark);	//해당 구역을 mark (다른 구역이면 mark 증가)
 		}
 	}
@@ -37,42 +40,42 @@ void findBridge(int n, int m, int row, int col, int mark, priority_queue<tp, vec
 	for (int i = 0; i < 4; i++) {	//상하좌우로 계속 다리 만들어보기
 		int tr = row + dr[i], tc = col + dc[i], dist = 0;	//다음 다리가 될 좌표, 다리의 길이
 		while (tr >= 0 && tr < n && tc >= 0 && tc < m) {	//좌표가 범위 내라면
-			if (board[tr][tc]) {	
+			if (board[tr][tc]) {	//어디든 섬과 닿았다면
 				if (board[tr][tc] != mark && dist > 1)		//새로운 섬 && 거리가 1보다 크다면
 					pq.push({ dist, mark, board[tr][tc] });	//우선순위 큐에 삽입
-				break;
+				break;	//다른 섬에 닿았으면 더 이상 현재 방향으로 다리를 늘리지 않음 (?)
 			}
-			tr += dr[i];	//현재 방향으로 다리 늘리기
-			tc += dc[i];	//현재 방향으로 다리 늘리기
+			tr += dr[i];	//현재 방향으로 row 전진
+			tc += dc[i];	//현재 방향으로 col 전진
 			dist++;			//다리 길이 늘리기
 		}
 	}
 }
 
-priority_queue<tp, vector<tp>, greater<>> buildBridge(int n, int m) {	//다리 짓고 
+priority_queue<tp, vector<tp>, greater<>> buildBridge(int n, int m) {	//가능한 모든 다리로 우선순위 큐 구성
 	priority_queue<tp, vector<tp>, greater<>> pq;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			if (board[i][j])	//섬이라면
-				findBridge(n, m, i, j, board[i][j], pq);
+				findBridge(n, m, i, j, board[i][j], pq);	//다른 섬까지의 다리 -> 우선순위 큐
 		}
 	}
-	return pq;
+	return pq;	//가능한 모든 다리가 저장된 우선순위 큐
 }
 
 //Find 연산
 int findParent(int node) {
-	if (parent[node] < 0)	//값이 음수면 루트 정점
-		return node;
+	if (parent[node] < 0)	//값이 음수면
+		return node;		//루트 정점
 	return parent[node] = findParent(parent[node]);	//그래프 압축하며 루트 정점 찾기
 }
 
 //Union 연산
 bool unionInput(int x, int y) {
-	int xp = findParent(x);
-	int yp = findParent(y);
+	int xp = findParent(x);	//x의 루트
+	int yp = findParent(y);	//y의 루트
 
-	if (xp == yp)
+	if (xp == yp)	//같은 집합이라면 유니온할 수 없음
 		return false;
 	if (parent[xp] < parent[yp]) { //새로운 루트 xp
 		parent[xp] += parent[yp];
@@ -85,13 +88,13 @@ bool unionInput(int x, int y) {
 	return true;
 }
 
-int kruskal(int v, priority_queue<tp, vector<tp>, greater<>> &pq) {
-	int cnt = 0, sum = 0;
+int kruskal(int v, priority_queue<tp, vector<tp>, greater<>> &pq) {	//크루스칼
+	int cnt = 0, sum = 0;	//간선의 개수, 가중치의 합
 
 	while (!pq.empty() && cnt < v - 1) {	//큐에 간선이 있고, 사용한 간선의 수가 v-1보다 적을 동안
-		int cost = get<0>(pq.top());		//가중치
-		int x = get<1>(pq.top());			
-		int y = get<2>(pq.top());			
+		int cost = get<0>(pq.top());		//시작점으로부터의 가중치
+		int x = get<1>(pq.top());			//정점 x좌표
+		int y = get<2>(pq.top());			//정점 y좌표
 
 		pq.pop();
 		if (unionInput(x, y)) { //유니온 했다면
@@ -131,6 +134,12 @@ int main() {
 
 	//연산
 	int node = markIsland(n, m);	//(섬의 개수 + 1)이 반환됨
+	/*for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++)
+			cout << board[i][j] << " ";
+		cout << endl;
+	}*/
+
 	parent.assign(node + 1, -1);
 	priority_queue<tp, vector<tp>, greater<>> pq = buildBridge(n, m);
 
